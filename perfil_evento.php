b3ff45712bfcb3778b53ece47c454d53e0874cd4
Bianca Victória Leithold de Oliveira <biancav.leithold@gmail.com>
2019-09-04 09:28:50 -0300

    finalmente comecei a tabela de despesas

--- perfil_evento.php
+++ perfil_evento.php
@@ -139,48 +139,7 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'salvar'  && $_REQUEST['id'] 
 <!--FIM BLOCO DE SALVAR Eventos-->
 
 
-
-<!--INICIO BLOCO MOSTRA DADOS Convidados -->
-<table class="ui fixed table" style="width: 45%; margin-left: 3%;">
-  <h2 class="header" style="margin-bottom: 2%; margin-left: 2%;">Convidados</h2>
-    <tr>
-        <th style="width: 75%;">Nome</th>
-        <th>Idade</th>
-    </tr> 
-<?php
-
-    try {
- 
-    $stmt = $connect->prepare("SELECT id_convidado,nome, idade FROM convidados WHERE id_evento = :id");
- 
-        if ($stmt->execute(array(
-          ':id' => $_REQUEST['id']))) {
-            while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {                
-                echo "<tr>";
-                echo "<td>".utf8_encode($rs->nome)."</td><td>".$rs->idade."<a href=\"?acao=upd&id=".$rs->id_convidado."\">"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<i class='pencil alternate icon'></i></a>"
-                           ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                           ."<a href=\"?acao=del&id=".$rs->id_convidado."\"><i class='trash alternate icon'></i>"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                           ."</a></center></td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "Erro: Não foi possível recuperar os dados do banco de dados";
-        }
-} catch (PDOException $erro) {
-    echo "Erro: ".$erro->getMessage();
-}
-?>
-  </table>
-  <?php
-  echo "<a href=?acao=insere&id=".$_REQUEST['id'].">";?>
-    <button class="ui blue basic button" style="margin-left: 35%">
-      <i class="icon plus"></i>
-        Adicionar Convidado
-    </button>
-  </a>
-<!--FIM BLOCO MOSTRA DADOS Convidados -->
-
-
+<section style="float: left; width: 50%;">
 <!--INICIO BLOCO ALTERAR E SALVAR Convidados -->
 <?php 
 if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'upd'  && $_REQUEST['id'] != '' ) {
@@ -194,9 +153,8 @@ if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'upd'  && $_REQUEST['id'] !
 
                 echo "<tr>";
                 echo "<td>";?> 
-                <br><br><br>
-                <h2 style="margin-left: 2%;">Alterar Dados Convidado</h2>
-                <form method="POST" action="?acao=save" style="width: 45.2%; margin-left: 3%;">
+                <h2 style="margin-left: 4%;">Alterar Dados Convidado</h2>
+                <form method="POST" action="?acao=save" style="width: 90%; margin-left: 6%;">
                   <div class="ui form">
                     <div class="fields">
                       <input type="hidden" name="id" value="<?php echo $rs->id_convidado ?>"/>
@@ -236,6 +194,50 @@ if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'save'  && $_REQUEST['id'] 
 }
 ?>
 <!--FIM BLOCO ALTERAR E SALVAR Convidados -->
+
+
+
+
+
+<!--INICIO BLOCO MOSTRA DADOS Convidados -->
+<table class="ui fixed table" style="width: 91.8%; margin-left: 6%;">
+  <h2 class="header" style="margin-bottom: 2%; margin-left: 4%;">Convidados</h2>
+    <tr>
+        <th style="width: 75%;">Nome</th>
+        <th>Idade</th>
+    </tr> 
+<?php
+
+    try {
+ 
+    $stmt = $connect->prepare("SELECT id_convidado,nome, idade FROM convidados WHERE id_evento = :id");
+ 
+        if ($stmt->execute(array(
+          ':id' => $_REQUEST['id']))) {
+            while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {                
+                echo "<tr>";
+                echo "<td>".utf8_encode($rs->nome)."</td><td>".$rs->idade."<a href=\"?acao=upd&id=".$rs->id_convidado."\">"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<i class='pencil alternate icon'></i></a>"
+                           ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                           ."<a href=\"?acao=del&id=".$rs->id_convidado."\"><i class='trash alternate icon'></i>"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                           ."</a></center></td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "Erro: Não foi possível recuperar os dados do banco de dados";
+        }
+} catch (PDOException $erro) {
+    echo "Erro: ".$erro->getMessage();
+}
+?>
+  </table>
+  <?php
+  echo "<a href=?acao=insere&id=".$_REQUEST['id'].">";?>
+    <button class="ui blue basic button" style="float: right; margin-right: 2.5%;">
+      <i class="icon plus"></i>
+        Adicionar Convidado
+    </button>
+  </a>
+<!--FIM BLOCO MOSTRA DADOS Convidados -->
 
 
 
@@ -301,6 +303,177 @@ if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'insere'  && $_REQUEST['id'
 
 ?>
 <!--FIM BLOCO INSERE Convidados -->
+</section>
+
+
+<section style="float: right; width: 50%;">
+
+<!--INICIO BLOCO ALTERAR E SALVAR DESPESA -->
+<?php 
+if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'upd'  && $_REQUEST['id'] != '' ) {
+  
+  $stmt = $connect->prepare("SELECT id_convidado,nome, idade FROM convidados WHERE id_convidado=:id");
+  $stmt->execute(array(
+    ':id' => $_REQUEST['id'],
+  )); 
+
+   while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {                
+
+                echo "<tr>";
+                echo "<td>";?> 
+                <h2>Alterar Dados Convidado</h2>
+                <form method="POST" action="?acao=save" style="width: 90%; margin-left: 2%;">
+                  <div class="ui form">
+                    <div class="fields">
+                      <input type="hidden" name="id" value="<?php echo $rs->id_convidado ?>"/>
+                      <div class="twelve wide field">
+                        <input type="text" name="nome" value="<?php echo utf8_encode($rs->nome) ?>"/><br>
+                      </div>
+                      <div class="three wide field">
+                        <input type="text" name="idade" value="<?php echo $rs->idade ?>"/>
+                      </div>
+                      <div>
+                        <input type="button" name="save" value="Cancelar" class="ui inverted red button"/>
+                      </div>
+                      <div class="field">
+                        <input type="submit" name="save" value="Salvar" class="ui inverted green button"/>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                      <?php
+                echo "</td>";
+                echo "</tr>";
+            }
+}
+?>
+
+<?php 
+if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'save'  && $_REQUEST['id'] != '' ) {
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+    $stmt = $connect->prepare("UPDATE convidados SET nome=:nome, idade=:idade WHERE id_convidado=:id");
+    $stmt->execute(array(
+      ':id' => $_REQUEST['id'],
+      ':nome' => $nome,
+      ':idade' => $idade
+  )); 
+}
+?>
+<!--FIM BLOCO ALTERAR E SALVAR DESPESAS -->
+
+
+
+
+
+<!--INICIO BLOCO MOSTRA DADOS DESPESAS -->
+<table class="ui fixed table" style="width: 91.8%; margin-left: 2%;">
+  <h2 class="header" style="margin-bottom: 2%;">Despesas</h2>
+    <tr>
+        <th style="width: 75%;">Empresa</th>
+        <th>Valor do Contrato</th>
+    </tr> 
+<?php
+
+    try {
+ 
+    $stmt = $connect->prepare("SELECT id_convidado,nome, idade FROM convidados WHERE id_evento = :id");
+ 
+        if ($stmt->execute(array(
+          ':id' => $_REQUEST['id']))) {
+            while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {                
+                echo "<tr>";
+                echo "<td>".utf8_encode($rs->nome)."</td><td>".$rs->idade."<a href=\"?acao=upd&id=".$rs->id_convidado."\">"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".""."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<i class='pencil alternate icon'></i></a>"
+                           ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                           ."<a href=\"?acao=del&id=".$rs->id_convidado."\"><i class='trash alternate icon'></i>"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                           ."</a></center></td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "Erro: Não foi possível recuperar os dados do banco de dados";
+        }
+} catch (PDOException $erro) {
+    echo "Erro: ".$erro->getMessage();
+}
+?>
+  <td style="font-size: 150%;"><b>Saldo Atual</b></td>
+  </table>
+  <?php
+  echo "<a href=?acao=insere&id=".$_REQUEST['id'].">";?>
+    <button class="ui blue basic button" style="float: right; margin-right: 6.5%;">
+      <i class="icon plus"></i>
+        Adicionar Despesa
+    </button>
+  </a>
+<!--FIM BLOCO MOSTRA DADOS DESPESAS -->
+
+
+
+<!--BLOCO EXCLUIR DADOS DESPESAS -->
+<?php
+  if (isset($_REQUEST["acao"]) && $_REQUEST["acao"] == "del" && $_REQUEST['id'] != '') {
+    try {
+        $stmt = $connect->prepare("DELETE FROM convidados WHERE id_convidado=:id");
+        $stmt->execute(array(
+          ':id' => $_REQUEST['id'],
+        ));
+    }catch (PDOException $erro) {
+      echo "Erro: ".$erro->getMessage();
+    }
+  }
+?>
+
+<!-- FIM DO BLOCO EXCLUIR DADOS DESPESAS -->
+
+
+<!-- INICIO BLOCO INSERE DESPESAS -->
+<?php
+if (isset($_REQUEST['acao']) && $_REQUEST['acao'] == 'insere'  && $_REQUEST['id'] != '' ) {
+
+  if(!isset($_POST['nome_ins'])) {
+            
+                echo "<tr>";
+                echo "<td>";?>
+                 <br><br><br>
+                 <h2 style="margin-left: 2%;">Adicionar Despesa</h2>
+                 <form method="POST" action="?acao=insere" style="width: 45.2%; margin-left: 3%;">
+                    <div class="ui form">
+                      <div class="fields">
+                        <input type="hidden" name="id" value="<?php echo $_REQUEST['id'] ?>"/>
+                          <div class="twelve wide field">
+                              <td><label>Nome do Convidado</label><input type="text" name="nome_ins"/></td>
+                          </div>
+                          <div class="three wide field">
+                              <td><label>Idade</label><input type="text" name="idade_ins"/></td>
+                          </div>
+                          <div>
+                              <input type="submit" name="insere" value="Adicionar" class="ui inverted green button" />
+                          </div>
+                      </div>
+                    </div>
+                <?php 
+                echo "</td>";
+                echo "</tr>";
+  } else {
+    
+      try{
+            $stmt = $connect->prepare('INSERT INTO  convidados (idade, nome, id_evento) VALUES (:idade , :nome, :id_evento)');
+            $stmt->execute(array(
+              ':idade' => $_REQUEST['idade_ins'],
+              ':nome' => $_REQUEST['nome_ins'],
+              ':id_evento' => $_REQUEST['id']
+            )); 
+          }catch (PDOException $erro) {
+              echo "Erro: ".$erro->getMessage();
+          }
+        }
+  }
+
+?>
+<!--FIM BLOCO INSERE DESPESAS -->
+</section>
+
 
 <?php
 include "rodape.php";
