0245901c5fe929825a09580b23e2cf6b41715a07
Vitor Leonardo Esser <vitoresser10@gmail.com>
2019-09-27 15:13:31 -0300

    terminando barra de pesquisa

--- pesquisar.php
+++ pesquisar.php
@@ -9,140 +9,137 @@
     $nome = $rs['nome'];
     $descricao = $rs['descricao'];
   }
-  $stmt = $connect->query('SELECT id_empresa, nome, descricao FROM empresa WHERE id_empresa=":id"');
-  while ($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $id_categoria = $rs['id_empresa']; 
-    $nome = $rs['nome'];
-    $descricao = $rs['descricao'];
-  }
+
 ?>
 
 <?php
       if(isset($_POST['pesquisar'])) {
    
-        $pesquisa = $connect->prepare('SELECT nome, cidade FROM empresa  WHERE nome LIKE :pesquisar' );
+        $pesquisa = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE nome LIKE :pesquisar');
         $pesquisa->execute(array(
-          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
+          ':pesquisar' => '%'.$_POST["pesquisar"].'%'
         ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-        // var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
-          
-         <table class="ui orange table" style="width:30%; float:none;">
-            <thead>
-                <tr>
-                    <th><?php echo utf8_encode($key);?></th>
-                </tr>
-                
-            </thead>
-            <tbody>
-                <tr>
-                <td> <a href="eventos.php?acao=&id=id_empresa"><?php echo utf8_encode($item); ?></a></td>
-                     
-                </tr>
-            </tbody>
-
-        </table>
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
+        <table class="ui orange table" style="width:30%; float:none;">
+          <thead>
+            <tr>
+              <th>Empresas</th>
+            </tr>
+          </thead>
+         
+        <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
+          <tbody>
+            <tr>
+              <td> <?php echo '<a href="perfil_empresa.php?id='.$item['id_empresa'].'">'.utf8_encode($item['nome']); ?></a></td>
+            </tr>
+          </tbody>
 
          <?php 
         
-        }
+        } ?>
+        </table>
+
+        <?php
       }
     ?>
     <?php
       if(isset($_POST['pesquisar'])) {
    
-        $pesquisa = $connect->prepare('SELECT cidade, nome FROM empresa  WHERE cidade LIKE :pesquisar' );
+        $pesquisa = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE cidade LIKE :pesquisar');
         $pesquisa->execute(array(
-          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
+          ':pesquisar' => '%'.$_POST["pesquisar"].'%'
         ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-        // var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
-          
-         <table class="ui orange table" style="width:30%; float:none;">
-            <thead>
-                <tr>
-                    <th><?php echo utf8_encode($key);?></th>
-                </tr>
-                
-            </thead>
-            <tbody>
-                <tr>
-                <td> <a href="eventos.php"><?php echo utf8_encode($item); ?></a></td>
-                     
-                </tr>
-            </tbody>
-
-        </table>
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
+        <table class="ui orange table" style="width:30%; float:none;">
+          <thead>
+            <tr>
+              <th>Empresas</th>
+            </tr>
+          </thead>
+         
+        <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
+          <tbody>
+            <tr>
+              <td> <?php echo '<a href="perfil_empresa.php?id='.$item['id_empresa'].'">'.utf8_encode($item['cidade']); ?></a></td>
+            </tr>
+          </tbody>
 
          <?php 
         
-        }
+        } ?>
+        </table>
+
+        <?php
       }
     ?>
-    
-    <?php
+
+  <?php
     if(isset($_POST['pesquisar'])) {
        
-       $pesquisa = $connect->prepare('SELECT nome_evento, descricao FROM eventos  WHERE nome_evento LIKE :pesquisar');
+       $pesquisa = $connect->prepare('SELECT id_evento, nome_evento, descricao FROM eventos  WHERE nome_evento LIKE :pesquisar');
        $pesquisa->execute(array(
          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
        ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-         var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
           
          <table class="ui orange table" style="width:30%; float:left; margin-left: 10%;" >
             <thead>
     
                 <tr>
-                    <th ><?php echo utf8_encode($key);?> </th>
+                <th>Eventos</th>
                    
                 </tr>
               
             </thead>
-  
+            <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
             <tbody>
                 <tr>
-                     <td> <a href="eventos.php"><?php echo utf8_encode($item); ?></a></td>
+                     <td> <?php echo '<a href="eventos.php?id='.$item['id_evento'].'">'.utf8_encode($item['nome_evento']); ?></a></td>
                     
                 </tr>
                
             </tbody>
 
-        </table>
-
-         <?php 
+            <?php 
         
-        }
-      }
-    ?>
+      } ?>
+      </table>
+
+      <?php
+    }
+  ?>
     <?php
     if(isset($_POST['pesquisar'])) {
        
-       $pesquisa = $connect->prepare('SELECT descricao, nome_evento FROM eventos  WHERE descricao LIKE :pesquisar');
+       $pesquisa = $connect->prepare('SELECT id_evento, descricao, nome_evento FROM eventos  WHERE descricao LIKE :pesquisar');
        $pesquisa->execute(array(
          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
        ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-        // var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
-          
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
+
          <table class="ui orange table" style="width:30%; float:left;  margin-left: 10%;">
             <thead>
                 <tr>
-                    <th><?php echo utf8_encode($key);?></th>
+                     <th>Descrição Eventos</th>
                 </tr>
                 
             </thead>
+            <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
             <tbody>
                 <tr>
-                <td> <a href="eventos.php"><?php echo utf8_encode($item); ?></a></td>
+                <td> <?php echo '<a href="eventos.php?id='.$item['id_evento'].'">'.utf8_encode($item['descricao']); ?></a></td>
                      
                 </tr>
             </tbody>
@@ -158,36 +155,36 @@
     <?php
       if(isset($_POST['pesquisar'])) {
    
-        $pesquisa = $connect->prepare('SELECT nome, descricao FROM categoria_evento  WHERE nome LIKE :pesquisar');
+        $pesquisa = $connect->prepare('SELECT id_categoria, nome, descricao FROM categoria_evento  WHERE nome LIKE :pesquisar');
         $pesquisa->execute(array(
           ':pesquisar' => '%'.$_POST['pesquisar'].'%'
         ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-        // var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
-          
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
          <table class="ui orange table" style="width:30%; float:left; margin-left: 10%;">
             <thead>
                 <tr>
-                    <th><?php echo utf8_encode($key);?></th>
+                <th>Categoria Evento</th>
                 </tr>
                 
             </thead>
+            <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
             <tbody>
                 <tr>
-                <td> <a href="eventos.php"><?php echo utf8_encode($item); ?></a></td>
+                <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['nome']); ?></a></td>
                      
                 </tr>
             </tbody>
 
         </table>
 
-         <?php 
+        <?php 
         
-        }
       }
-    ?>
+    }
+  ?>
     <?php
       if(isset($_POST['pesquisar'])) {
    
@@ -195,34 +192,34 @@
         $pesquisa->execute(array(
           ':pesquisar' => '%'.$_POST['pesquisar'].'%'
         ));
-        $resultado_pesquisa = $pesquisa->fetch(PDO::FETCH_ASSOC);
-        // var_dump($resultado_pesquisa);
-       
-        foreach ($resultado_pesquisa as $key => $item) {?>
+        $resultado_pesquisa = $pesquisa->fetchAll();
+        ?>
           
          <table class="ui orange table" style="width:30%; float:left; margin-left: 10%;">
             <thead>
                 <tr>
-                    <th><?php echo utf8_encode($key);?></th>
+                <th>Descrição Categoria</th>
                 </tr>
                 
             </thead>
+            <?php
+        foreach ($resultado_pesquisa as $key => $item ) {
+        ?>
             <tbody>
                 <tr>
-                <td> <a href="eventos.php"><?php echo utf8_encode($item); ?></a></td>
+                <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['descricao']); ?></a></td>
                      
                 </tr>
             </tbody>
 
         </table>
 
-         <?php 
+        <?php 
         
-        }
       }
-    ?>
-  </div>
-  </div>
+    }
+  ?>
+ 
 
 
 <?php
