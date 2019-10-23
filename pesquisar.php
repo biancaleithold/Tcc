ecf570020a47b639253be7b1cf31a6efedd8f1ce
VitorEsser <vitoresser10@gmail.com>
2019-10-23 10:12:10 -0300

    alterei pesquisa

--- pesquisar.php
+++ pesquisar.php
@@ -15,23 +15,130 @@
 <?php
       if(isset($_POST['pesquisar'])) {
    
-        $pesquisa = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE nome LIKE :pesquisar');
-        $pesquisa->execute(array(
+        $pesquisa1 = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE nome LIKE :pesquisar');
+        $pesquisa1->execute(array(
           ':pesquisar' => '%'.$_POST["pesquisar"].'%'
         ));
-        $resultado_pesquisa = $pesquisa->fetchAll();
+        $resultado_pesquisa1 = $pesquisa1->fetchAll();
         ?>
-        <?php  if (!empty($resultado_pesquisa)) { ?>
+        <?php  if (!empty($resultado_pesquisa1)) { ?>
+           
         <table class="ui orange table" style="width:30%; float:left; margin-left: 10%;">
           <thead>
             <tr>
               <th>Empresas</th>
             </tr>
           </thead>
-          <?php } ?>
+          <?php }elseif(isset($_POST['pesquisar'])) {
+   
+           $pesquisa2 = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE cidade LIKE :pesquisar');
+            $pesquisa2->execute(array(
+     ':pesquisar' => '%'.$_POST["pesquisar"].'%'
+   ));
+   $resultado_pesquisa2 = $pesquisa2->fetchAll();
+   ?>
+   <?php  if (!empty($resultado_pesquisa2)) { ?>
+   
+   <table class="ui orange table" style="width:30%;float:left; margin-left: 10%;">
+     <thead>
+       <tr>
+         <th>Empresas</th>
+       </tr>
+     </thead>
+     <?php }elseif(isset($_POST['pesquisar'])) {
+
+$pesquisa3 = $connect->prepare('SELECT id_categoria, nome, descricao FROM categoria_evento  WHERE nome LIKE :pesquisar');
+$pesquisa3->execute(array(
+':pesquisar' => '%'.$_POST['pesquisar'].'%'
+));
+$resultado_pesquisa3 = $pesquisa3->fetchAll();
+?>
+<?php  if (!empty($resultado_pesquisa3)) { ?>
+<table class="ui orange table" style="width:30%; float:left; margin-left: 10%;">
+  <thead>
+      <tr>
+      <th>Categoria Evento</th>
+      </tr>
+      
+  </thead>
+  <?php }elseif(isset($_POST['pesquisar'])) {
+
+
+$pesquisa4 = $connect->prepare('SELECT id_categoria, descricao, nome FROM categoria_evento  WHERE descricao LIKE :pesquisar');
+$pesquisa4->execute(array(
+':pesquisar' => '%'.$_POST['pesquisar'].'%'
+));
+$resultado_pesquisa4 = $pesquisa4->fetchAll();
+?>
+<?php  if (!empty($resultado_pesquisa4)) { ?>
+<table class="ui orange table" style="width:30%; float:right; margin-right: 20%;">
+  <thead>
+      <tr>
+      <th>Descrição Categoria</th>
+      </tr>
+      
+  </thead>
+<?php } else{
+  echo "<script>alert('Resultado não encontrado!')</script>";
+}?>
+  <?php
+foreach ($resultado_pesquisa4 as $key => $item ) {
+?>
+  <tbody>
+      <tr>
+
+      <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['descricao']);?></a></td>
+           
+      </tr>
+  </tbody>
+
+</table>
+
+
+<?php 
+}
+}
+  ?>
+
+  <?php
+foreach ($resultado_pesquisa3 as $key => $item ) {
+?>
+  <tbody>
+      <tr>
+      <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['nome']); ?></a></td>
+           
+      </tr>
+  </tbody>
+
+</table>
+
+<?php 
+}
+}
+       ?>
+  
+   <?php
+   foreach ($resultado_pesquisa2 as $key => $item ) {
+   ?>
+     <tbody>
+       <tr>
+         <td> <?php echo '<a href="perfil_empresa.php?id='.$item['id_empresa'].'">'.utf8_encode($item['cidade']); ?></a></td>
+       </tr>
+     </tbody>
+
+    <?php 
+    
+   } ?>
+   </table>
+
+   <?php
+ }
+          
+          ?>
         <?php
-        foreach ($resultado_pesquisa as $key => $item ) {
+        foreach ($resultado_pesquisa1 as $key => $item ) {
         ?>
+ 
           <tbody>
             <tr>
               <td> <?php echo '<a href="perfil_empresa.php?id='.$item['id_empresa'].'">'.utf8_encode($item['nome']); ?></a></td>
@@ -42,117 +149,11 @@
         
         } ?>
         </table>
-
+      
         <?php
       }
+      
     ?>
-    <?php
-      if(isset($_POST['pesquisar'])) {
    
-        $pesquisa = $connect->prepare('SELECT id_empresa, nome, cidade FROM empresa  WHERE cidade LIKE :pesquisar');
-        $pesquisa->execute(array(
-          ':pesquisar' => '%'.$_POST["pesquisar"].'%'
-        ));
-        $resultado_pesquisa = $pesquisa->fetchAll();
-        ?>
-        <?php  if (!empty($resultado_pesquisa)) { ?>
-        <table class="ui orange table" style="width:30%;float:left; margin-left: 10%;">
-          <thead>
-            <tr>
-              <th>Empresas</th>
-            </tr>
-          </thead>
-          <?php } ?>
-        <?php
-        foreach ($resultado_pesquisa as $key => $item ) {
-        ?>
-          <tbody>
-            <tr>
-              <td> <?php echo '<a href="perfil_empresa.php?id='.$item['id_empresa'].'">'.utf8_encode($item['cidade']); ?></a></td>
-            </tr>
-          </tbody>
-
-         <?php 
-        
-        } ?>
-        </table>
-
-        <?php
-      }
-    ?>
-
-
-    <?php
-      if(isset($_POST['pesquisar'])) {
-   
-        $pesquisa = $connect->prepare('SELECT id_categoria, nome, descricao FROM categoria_evento  WHERE nome LIKE :pesquisar');
-        $pesquisa->execute(array(
-          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
-        ));
-        $resultado_pesquisa = $pesquisa->fetchAll();
-        ?>
-        <?php  if (!empty($resultado_pesquisa)) { ?>
-         <table class="ui orange table" style="width:30%; float:left; margin-left: 10%;">
-            <thead>
-                <tr>
-                <th>Categoria Evento</th>
-                </tr>
-                
-            </thead>
-            <?php } ?>
-            <?php
-        foreach ($resultado_pesquisa as $key => $item ) {
-        ?>
-            <tbody>
-                <tr>
-                <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['nome']); ?></a></td>
-                     
-                </tr>
-            </tbody>
-
-        </table>
-
-        <?php 
-        
-        }
-    }
-  ?>
-    <?php
-      if(isset($_POST['pesquisar'])) {
-   
-
-        $pesquisa = $connect->prepare('SELECT id_categoria, descricao, nome FROM categoria_evento  WHERE descricao LIKE :pesquisar');
-        $pesquisa->execute(array(
-          ':pesquisar' => '%'.$_POST['pesquisar'].'%'
-        ));
-        $resultado_pesquisa = $pesquisa->fetchAll();
-        ?>
-        <?php  if (!empty($resultado_pesquisa)) { ?>
-         <table class="ui orange table" style="width:30%; float:right; margin-right: 20%;">
-            <thead>
-                <tr>
-                <th>Descrição Categoria</th>
-                </tr>
-                
-            </thead>
-        <?php } ?>
-            <?php
-        foreach ($resultado_pesquisa as $key => $item ) {
-        ?>
-            <tbody>
-                <tr>
-
-                <td> <?php echo '<a href="eventos.php?id='.$item['id_categoria'].'">'.utf8_encode($item['descricao']);?></a></td>
-                     
-                </tr>
-            </tbody>
-
-        </table>
-
-        <?php 
-        }
-      }
-  ?>
- 
-
-
+   <?php
+          ///if  ($resultado_pesquisa != $pesquisa){ echo "<script>alert('Resultado nao encontrado')</script>"; }?> 
